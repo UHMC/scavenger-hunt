7e31a09e891f16f0cbb2cb4b40f39ca1f2e9ea83
Mario Canul <mcanul@hawaii.edu>
2016-04-12 16:25:49 -1000

    Update AltBeaconLib to 2.8
    - Things broke tho

--- app/src/main/java/educybersecurity/hawaii/maui/uhmcscavengerhunt/MainActivity.java
+++ app/src/main/java/educybersecurity/hawaii/maui/uhmcscavengerhunt/MainActivity.java
@@ -3,15 +3,9 @@ package educybersecurity.hawaii.maui.uhmcscavengerhunt;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.RemoteException;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.util.Range;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -54,17 +48,17 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
             String UUID = Integer.toString(beacon.getServiceUuid());
             String Type = Integer.toString(beacon.getBeaconTypeCode());
             Log.d("Beacons","UUID is: "+UUID+" and Type is "+Type);
-            try{Log.d("Beacons",new String(beacon.getId1().toByteArray(),"UTF-8"));}catch(Exception e){}
-            if (beacon.getServiceUuid() == 0xfeaa /*&& beacon.getBeaconTypeCode() == 0x0*/) {
+            try{Log.d("Beacons",new String(beacon.getId1().toByteArray()));}catch(Exception e){}
+            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x10) {
                 // This looks for a Eddystone-UID frame, then checks to see what type it is.
                 // For our purposes, we use 0x10, which defines an Eddystone-URL
                 Identifier namespaceId = beacon.getId1();
-                Identifier instanceId = beacon.getId2();
+//                Identifier instanceId = beacon.getId2();
                 //Identifier testId=beacon.getId3();
                 //Log.d("Beacons","testId: "+testId);
 
                 Log.d("Beacons", "I see a beacon transmitting namespace id: " + namespaceId +
-                        " and instance id: " + instanceId +
+                        " and instance id: " + "NONE" +
                         " approximately " + beacon.getDistance() + " meters away.");
             }
         }
@@ -82,12 +76,12 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
 
     @Override
     public void onResume() {
-        Log.d("Beacons","Program actually gets here");
+        Log.d("Beacons", "Program actually gets here");
         super.onResume();
         mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
         // Detect the main Eddystone-UID frame:
         mBeaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout("s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-13,i:14-19"));
+                setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
         mBeaconManager.bind(this);
     }
 
