34bb6ed4fcc9eb563d2f42c73cf6b0e4c0e5aaf9
Mario Canul <mcanul@hawaii.edu>
2016-04-15 13:30:03 -1000

    Adds file to keep track of current step

--- app/src/main/java/educybersecurity/hawaii/maui/uhmcscavengerhunt/MainActivity.java
+++ app/src/main/java/educybersecurity/hawaii/maui/uhmcscavengerhunt/MainActivity.java
@@ -18,20 +18,21 @@ import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.utils.UrlBeaconUrlCompressor;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Collection;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
-    private static final int REQUEST_ENABLE_BT=1;
+    private static final int REQUEST_ENABLE_BT = 1;
     private int currentStop;
-    private static final String[] hexURLs={
-            "http://www.example.com/folder/file1.ext/",
+    private static final int RADIUS = 5;
+    private static final String[] hexURLs = {
+            "http://test1url.net",
             "http://www.example.com/folder/file2.ext/",
             "http://www.example.com/folder/file3.ext/",
             "http://www.example.com/folder/file4.ext/",
@@ -51,46 +52,109 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        TextView beaconCheck = new TextView(this);
+
+        // Creates a file for keeping track of steps, only happens once also initializes currentStop
+        FileWriter fw;
+        FileReader fr;
+        File file = new File(getFilesDir().getPath().toString() + "/prefs.txt");
+        try {
+            // Writes 0 at file creation (step 1)
+            file.createNewFile();
+            fw = new FileWriter(file, true);
+            fw.write(0);
+            fw.flush();
+            fw.close();
+
+            // Reads from file and initializes currentStop
+            fr = new FileReader(file);
+            currentStop = fr.read();
+            fr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 
-        BluetoothAdapter btAdapter=BluetoothAdapter.getDefaultAdapter();
-        if(btAdapter!=null&&!btAdapter.isEnabled()){
-            Intent enableBtIntent=new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent,REQUEST_ENABLE_BT);
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter != null && !btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==REQUEST_ENABLE_BT){
-            if(resultCode==RESULT_OK)Log.d("Beacons","Bluetooth enabled.");
-            else Log.d("Beacons","Bluetooth not enabled!");
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) Log.d("Beacons", "Bluetooth enabled.");
+            else Log.d("Beacons", "Bluetooth not enabled!");
         }
     }
 
     @Override
     public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-        for (Beacon beacon: beacons) {
+        // I/O stuff
+        FileWriter fw;
+        FileReader fr;
+        File file = new File(getFilesDir().getPath().toString() + "/prefs.txt");
+
+
+        for (Beacon beacon : beacons) {
             //Byte[] url=new Byte[]
             //Log.d("BeaconToString", UrlBeaconUrlCompressor.uncompress(beacon.getId1().toByteArray()));
             //Log.d("whatthefuck",UrlBeaconUrlCompressor.uncompress(UrlBeaconUrlCompressor.compress("http://www.something.net/folder/file.ext")));
-            try{
-                if(new String(UrlBeaconUrlCompressor.compress(hexURLs[currentStop]),"UTF-8").equals(new String(beacon.getId1().toByteArray(),"UTF-8"))){
+
+            try {
+
+                fr = new FileReader(file);
+                currentStop = fr.read();
+                fr.close();
+                Log.d("Step", Integer.toString(currentStop));
+            } catch (Exception e) {
+            }
+
+
+            try {
+                if (new String(UrlBeaconUrlCompressor.compress(hexURLs[currentStop]), "UTF-8").equals(new String(beacon.getId1().toByteArray(), "UTF-8"))) {
                     //NEXT STOP DETECTED
-                    if(beacon.getDistance()<=RADIUS){
+                    currentStop++;
+                    fw = new FileWriter(file, false);
+                    fw.write(currentStop);
+                    fw.flush();
+                    fw.close();
+                    Log.d("Step", Integer.toString(currentStop));
+
+                    if (beacon.getDistance() <= RADIUS) {
+
                         //USER HAS VISITED THE LOCATION
+
+                        switch (currentStop) {
+                            case 0:
+                                // update UI to step 1
+                                break;
+                            case 1:
+                                // update UI to step 2
+                                break;
+                            case 2:
+                                // update UI to step 3
+                                break;
+                            case 3:
+                                // update UI to step 4
+                                break;
+                        }
                     }
                 }
-            }catch(Exception e){};
+            } catch(Exception e){
         }
     }
+
+}
+
     public void onBeaconServiceConnect() {
-        Log.d("Beacons","How about here?");
+        Log.d("Beacons", "How about here?");
         Region region = new Region("all-beacons-region", null, null, null);
         try {
             mBeaconManager.startRangingBeaconsInRegion(region);
@@ -102,7 +166,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
 
     @Override
     public void onResume() {
-        Log.d("Beacons","Program actually gets here");
+        Log.d("Beacons", "Program actually gets here");
         super.onResume();
         mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
         // Detect the main Eddystone-UID frame:
